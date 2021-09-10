d7c56d8579027025d7aefd641b451b55bc9a35e2
vitor <vitorsbsilva@gmail.com>
2021-09-10 15:36:05 -0300

    requisito 4 - inserindo pixels 5x5 - primeira tentativa

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    let pixels = document.querySelectorAll("#pixel");
+    pixels.style.backgroundColor = "white"; 
+}
+
+/*
+function fillPixels(pixels){
+    for (let index = 0; index < pixels.length; index += 1) {
+        fourExtraPixels(pixels[index]);
+    }
+}
+
+function fourExtraPixels () {
+    for (let index = 0; index < 4; index += 1) {
+        let newPixels = document.createElement("div");
+        pixels.appendChild(newPixels);
+    }
+} */
